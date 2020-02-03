92af33852b049a0231e214c9255b6213cf010454
xushalx <xushalx@gmail.com>
2020-02-03 13:40:48 -0800

    Q moving window

--- algo.py
+++ algo.py
@@ -88,92 +88,124 @@
 
 #Circular Q
 
-class MyCircularQueue:
+# class MyCircularQueue:
 
-    def __init__(self, k: int):
-        """
-        Initialize your data structure here. Set the size of the queue to be k.
-        """
-        self.myQ=[None]*k
-        self.head=-1
-        self.tail=-1
-        self.size=k
+#     def __init__(self, k: int):
+#         """
+#         Initialize your data structure here. Set the size of the queue to be k.
+#         """
+#         self.myQ=[None]*k
+#         self.head=-1
+#         self.tail=-1
+#         self.size=k
 
-    def enQueue(self, value: int) -> bool:
-        """
-        Insert an element into the circular queue. Return true if the operation is successful.
-        """
-        if self.isFull():
-            return False
-        if self.isEmpty():
-            self.head=0
-        self.tail=(self.tail+1)%self.size
-        self.myQ[self.tail]=value
-        return True
+#     def enQueue(self, value: int) -> bool:
+#         """
+#         Insert an element into the circular queue. Return true if the operation is successful.
+#         """
+#         if self.isFull():
+#             return False
+#         if self.isEmpty():
+#             self.head=0
+#         self.tail=(self.tail+1)%self.size
+#         self.myQ[self.tail]=value
+#         return True
 
             
-    def deQueue(self) -> bool:
-        """
-        Delete an element from the circular queue. Return true if the operation is successful.
-        """
-        if self.isEmpty():
-            return False
-        if self.head==self.tail:
-            self.head=-1
-            self.tail=-1
-            return True
-        self.head=(self.head+1)%self.size
-        return True
+#     def deQueue(self) -> bool:
+#         """
+#         Delete an element from the circular queue. Return true if the operation is successful.
+#         """
+#         if self.isEmpty():
+#             return False
+#         if self.head==self.tail:
+#             self.head=-1
+#             self.tail=-1
+#             return True
+#         self.head=(self.head+1)%self.size
+#         return True
 
-    def Front(self) -> int:
-        """
-        Get the front item from the queue.
-        """
-        if self.isEmpty():
-            return self.head
-        return self.myQ[self.head]
+#     def Front(self) -> int:
+#         """
+#         Get the front item from the queue.
+#         """
+#         if self.isEmpty():
+#             return self.head
+#         return self.myQ[self.head]
 
-    def Rear(self) -> int:
-        """
-        Get the last item from the queue.
-        """
-        if self.isEmpty():
-            return self.tail
-        return self.myQ[self.tail]
+#     def Rear(self) -> int:
+#         """
+#         Get the last item from the queue.
+#         """
+#         if self.isEmpty():
+#             return self.tail
+#         return self.myQ[self.tail]
 
-    def isEmpty(self) -> bool:
-        """
-        Checks whether the circular queue is empty or not.
-        """
-        if self.head==-1:
-            return True
-        else:
-            return False
+#     def isEmpty(self) -> bool:
+#         """
+#         Checks whether the circular queue is empty or not.
+#         """
+#         if self.head==-1:
+#             return True
+#         else:
+#             return False
 
-    def isFull(self) -> bool:
-        """
-        Checks whether the circular queue is full or not.
-        """
-        if ((self.tail+1)%self.size)==self.head:
-            return True
-        else:
-            return False
+#     def isFull(self) -> bool:
+#         """
+#         Checks whether the circular queue is full or not.
+#         """
+#         if ((self.tail+1)%self.size)==self.head:
+#             return True
+#         else:
+#             return False
         
 
 
-# Your MyCircularQueue object will be instantiated and called as such:
+# # Your MyCircularQueue object will be instantiated and called as such:
 
-obj = MyCircularQueue(6)
-# print(len(obj))
-print(obj.enQueue(6))
-print(obj.Rear())
-print(obj.Rear())
-print(obj.deQueue())
-# print(len(obj.myQ))
-print(obj.enQueue(5))
-print(obj.Rear())
-print(obj.deQueue())
-print(obj.Front())
-print(obj.deQueue())
-print(obj.deQueue())
-print(obj.deQueue())
+# obj = MyCircularQueue(6)
+# # print(len(obj))
+# print(obj.enQueue(6))
+# print(obj.Rear())
+# print(obj.Rear())
+# print(obj.deQueue())
+# # print(len(obj.myQ))
+# print(obj.enQueue(5))
+# print(obj.Rear())
+# print(obj.deQueue())
+# print(obj.Front())
+# print(obj.deQueue())
+# print(obj.deQueue())
+# print(obj.deQueue())
+
+
+from queue import *
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.myQ=Queue(maxsize=size)
+        self.sum=0
+        self.maxSize=size
+        self.curSize=0
+
+    def next(self, val: int) -> float:
+        if self.myQ.full():
+            self.sum-=self.myQ.get()
+            self.sum+=val
+            self.curSize=self.maxSize
+            self.myQ.put(val)
+            return self.sum/self.curSize
+        self.sum+=val
+        self.curSize+=1
+        self.myQ.put(val)
+        return self.sum/self.curSize
+x= MovingAverage(3)
+z=Queue(maxsize=2)
+print(x.next(1))
+print(x.next(10))
+print(x.next(3))
+print(x.next(5))
